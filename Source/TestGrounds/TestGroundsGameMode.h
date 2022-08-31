e10b4bc860f152d0d6a503ef9daee8a63a28778e
Davilyyoung <hal.yang.yi.2021@gmail.com>
2022-08-31 22:49:03 +0900

    Initial commit

---
+++ Source/TestGrounds/TestGroundsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestGroundsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATestGroundsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATestGroundsGameMode();
+};
+
+
+
