26a852161fa3f484f00182ad1ec6daa7b455b35c
Davilyyoung <hal.yang.yi.2021@gmail.com>
2022-09-05 03:01:16 +0900

    Add PatrolRouteComponent and BTTask in C++

---
+++ Source/TestGrounds/Private/ChooseNextWaypoint.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "PatrolRouteComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Engine/TargetPoint.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	// BP 1 : Get The Partrol Route
+	auto ControllerPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControllerPawn->FindComponentByClass<UPatrolRouteComponent>();
+	if (!ensure(PatrolRoute)){return  EBTNodeResult::Failed;}
+	// Warn about empty patrol routes
+	auto Patrolpoints = PatrolRoute->GetPatrolPoint();
+	if (Patrolpoints.Num() == 0)
+	{
+		UE_LOG(LogTemp,Warning,TEXT("A guard is missing Patrol points"));
+		return  EBTNodeResult::Failed;
+	}
+	
+	// BP 2 : Set next WayPoint 
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName,Patrolpoints[Index]);
+	
+	// BP 3 : Cycle the index
+	auto NextIndex = (Index + 1) % Patrolpoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName,NextIndex);
+	
+	//UE_LOG(LogTemp,Warning,TEXT("Index %i"),Index);
+	//UE_LOG(LogTemp,Warning,TEXT("AI In C++"));
+	
+	return EBTNodeResult::Succeeded;
+}
--- Source/TestGrounds/TestGrounds.Build.cs
+++ Source/TestGrounds/TestGrounds.Build.cs
@@ -9,5 +9,6 @@ public class TestGrounds : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "GameplayTasks"});
 	}
 }
