cd32134da7fd725c2542057de5638a6106ce3ab9
Davilyyoung <hal.yang.yi.2021@gmail.com>
2022-10-14 16:38:23 +0900

    new Animation modify

---
+++ Source/TestGrounds/Public/Character/Mannequin.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Mannequin.generated.h"
+
+class USkeletalMeshComponent;
+class UCameraComponent;
+class AGun;
+
+
+UCLASS()
+class TESTGROUNDS_API AMannequin : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMannequin();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+	UPROPERTY(EditDefaultsOnly,Category= "Setup")
+	TSubclassOf<AGun> GunBlueprint;
+
+	UFUNCTION(BlueprintCallable,Category= "Weapon")
+	void PullTrigger();
+
+private:
+	UPROPERTY(VisibleDefaultsOnly,Category = "Mesh")
+	USkeletalMeshComponent* Mesh1PComp;
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category = "Camera",meta=(AllowPrivateAccess = "ture"))
+	UCameraComponent* FirstPersonCameraComp;
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category = "Camera",meta=(AllowPrivateAccess = "ture"))
+	AGun* Gun;
+
+
+	
+};
